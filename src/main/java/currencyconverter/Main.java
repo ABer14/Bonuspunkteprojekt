088abc79a89aa256be68a2264b4c4d04ab013e19
ABer14 <bernardoabeljose@gmail.com>
2023-02-02 17:50:56 +0100

    new main methode

--- src/main/java/currencyconverter/Main.java
+++ src/main/java/currencyconverter/Main.java
@@ -1,7 +1,13 @@
-package org.example;
+package currencyconverter;
+
+//Projekt von ab327 und ff0061
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CurrencyTable.createCurrency(); // Create the currency table
+        Converter.converterRun(); // Run the converter
+
     }
+
 }
